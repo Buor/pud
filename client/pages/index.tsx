95cd7d52dfe7c6f81330226b47cbe59c4e0d26d9
Buor <buor0@mail.ru>
2021-11-07 09:35:29 +0300

    feat: next js set up

---
+++ client/pages/index.tsx
@@ -0,0 +1,12 @@
+import type {NextPage} from 'next'
+import styles from './../src/styles/Home.module.scss'
+
+const Home: NextPage = () => {
+    return (
+        <div className={styles.wrapper}>
+
+        </div>
+    )
+}
+
+export default Home
