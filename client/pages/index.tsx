432f3c534096f9e45b31051f221aec9c6630b1f5
Buor <buor0@mail.ru>
2021-11-08 09:01:42 +0300

    feat: main application page HTML done
    - Also added raleway font variants

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,10 +1,27 @@
 import type {NextPage} from 'next'
 import styles from './../src/styles/Home.module.scss'
+import {useState} from "react"
 
 const Home: NextPage = () => {
+    const [isButtonHovered, setIsButtonHovered] = useState(false)
+
     return (
         <div className={styles.wrapper}>
-
+            <div className={styles.logo_wrapper}>
+                <div className={styles.title}>
+                    PUD
+                </div>
+                <div className={styles.subtitle}>
+                    Power-Up Diary
+                </div>
+            </div>
+            <div className={styles.btn_wrapper + " " + (isButtonHovered ? styles.btn_hovered : "")}>
+                <button
+                    onMouseEnter={() => setIsButtonHovered(true)}
+                    onMouseLeave={() => setIsButtonHovered(false)}
+                    className={styles.btn_enter}>Press to enter
+                </button>
+            </div>
         </div>
     )
 }
